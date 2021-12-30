ef66d9b8da903c69ee2c7ea321c02e942aad9c98
julianacochenski <juliana.cochenski@gmail.com>
2021-12-30 16:53:29 +0100

    Create Button.vue component.

---
+++ ping/src/components/atoms/Button.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <button class="button-unit">Notify Me</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ButtonUnit"
+}
+</script>
+
+<style lang="scss">
+.button-unit {
+  width: 100%;
+  height: 75rem;
+  padding: 10rem;
+  margin: 10rem;
+  font-size: 25rem;
+  font-weight: 600;
+  text-align: center;
+  color: white;
+  background-color: var(--color-blue-60);
+  border-radius: 45rem;
+
+  @include media('>=600') {
+    width: 200rem;
+    height: 55rem;
+    font-size: 16rem;
+  }
+}
+</style>
