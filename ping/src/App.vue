fbb97c54d644c97ed57829e87ce36d039a7290e7
julianacochenski <juliana.cochenski@gmail.com>
2021-12-31 15:03:40 +0100

    Create heater-unit.vue component.

--- ping/src/App.vue
+++ ping/src/App.vue
@@ -1,18 +1,39 @@
 <template>
-  <ButtonUnit />
+  <div class="app">
+    <header-unit class="app__header-unit" />
+    <ButtonUnit />
+
+  </div>
 </template>
 
 <script>
 import ButtonUnit from "./components/atoms/Button.vue";
+import HeaderUnit from "./components/atoms/header-unit";
 
 export default {
   name: 'App',
   components: {
+    HeaderUnit,
     ButtonUnit,
   }
 }
 </script>
 
 <style lang="scss">
+.app {
 
+  @include media('>=600') {
+    display: grid;
+    grid-template-rows: 100rem 1fr 100rem;
+    grid-template-columns: 1fr;
+    grid-template-areas:
+    "header-unit"
+    "main"
+    "footer-unit";
+  }
+
+  &__header-unit {
+    grid-area: header-unit;
+  }
+}
 </style>
---
+++ ping/src/components/atoms/header-unit.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="header-unit">
+    <img src="@/assets/img/icons/logo.svg" alt="logo">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "header-unit"
+}
+</script>
+
+<style lang="scss">
+.header-unit {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
