614235bb3a0f6bae41addf4585dd09b4fe3c3c89
julianacochenski <juliana.cochenski@gmail.com>
2022-01-03 17:31:35 +0100

    Create icon-unit.vue.

---
+++ ping/src/components/atoms/icon-unit.vue
@@ -0,0 +1,24 @@
+<template>
+  <a class="icon-unit" href="#" >
+    <img
+        class="icon-unit__image"
+        :src="require(`@/assets/img/icons/${name}.svg`)"
+        :alt="alt"
+    />
+  </a>
+
+</template>
+
+<script>
+export default {
+  name: "icon-unit",
+  props: {
+    name: String,
+    alt: String
+  },
+}
+</script>
+
+<style lang="scss">
+
+</style>
