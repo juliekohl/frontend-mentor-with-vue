1cd1ea55f80de8e3dd1d32cd111537d525d1a7de
julianacochenski <juliana.cochenski@gmail.com>
2022-01-06 19:10:30 +0100

    Setup project.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <HelloWorld />
+  </div>
 </template>
 
 <script>
@@ -14,13 +15,8 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+.app {
+  background-color: aqua;
 }
 </style>
