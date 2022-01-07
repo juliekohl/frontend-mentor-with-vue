73a14afe39709337a773388ec618242f90a90fd8
julianacochenski <juliana.cochenski@gmail.com>
2022-01-07 15:03:27 +0100

    Setup new project.

---
+++ testimonials/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app">
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
+.app {
+  background-color: aqua;
+}
+</style>
---
+++ testimonials/src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    <h1>Hello Julie!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
