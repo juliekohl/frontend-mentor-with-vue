99c7143884a1e5e031ba34184e1cfa8a70cde76a
julianacochenski <juliana.cochenski@gmail.com>
2022-01-07 21:24:05 +0100

    Create image-unit.vue component.

---
+++ testimonials/src/components/atoms/image-unit.vue
@@ -0,0 +1,25 @@
+<template>
+  <img 
+      class="photo" 
+      :src="require(`@/assets/images/${photo}.jpg`)"
+      :alt="alt"
+  />
+</template>
+
+<script>
+export default {
+  name: "image-unit",
+  props: {
+    photo: String,
+    alt: String
+  }
+}
+</script>
+
+<style lang="scss">
+.photo {
+  display: block;
+  width: 250px;
+  margin: 0 auto;
+}
+</style>
